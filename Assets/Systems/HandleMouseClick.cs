ca268d55f447408073d2acc3aa7f7600fe6e765c
foxx553 <delhonf@gmail.com>
2025-02-02 22:04:10 -0500

    Fix click hold bug

--- Assets/Systems/HandleMouseClick.cs
+++ Assets/Systems/HandleMouseClick.cs
@@ -20,6 +20,12 @@ public class HandleMouseClick : ISystem
         if (Input.GetMouseButtonDown(0)) {
             var mouse3DScreenPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             var mouse2DScreenPosition = new Vector2(mouse3DScreenPosition.x, mouse3DScreenPosition.y);
+
+            List<Vector2> newPositions = new List<Vector2>();
+            List<Vector2> newVelocities = new List<Vector2>();
+            var newSize = 0;
+            bool explodedCircle = false;
+
             for (uint id = 0; id < _componentDatabase.entitiesCounter; id ++) {
                 if (_componentDatabase.positionComponents[id] == null) continue;
 
@@ -35,10 +41,8 @@ public class HandleMouseClick : ISystem
                         _componentDatabase.DestroyId(id);
                         _componentDatabase.availableIds.Add(id);
                     } else {
-                        List<Vector2> newPositions = new List<Vector2>();
-                        List<Vector2> newVelocities = new List<Vector2>();
 
-                        var newSize = (int) Math.Ceiling(_componentDatabase.sizeComponents[id].Size / 4.0);
+                        newSize = (int) Math.Ceiling(_componentDatabase.sizeComponents[id].Size / 4.0);
                         var newPositionOffset = (float) (newSize / 1.9); // Instead of 2.0, to prevent unwanted collision
                         var currentPosition = _componentDatabase.positionComponents[id].Position;
                         var currentVelocityMagnitude = _componentDatabase.velocityComponents[id].Velocity.magnitude;
@@ -62,29 +66,34 @@ public class HandleMouseClick : ISystem
                         newVelocities.Add(new Vector2((float) -finalVelocity, (float) finalVelocity));
                         finalVelocity = newVelocityOffset * (0.5 + random.NextDouble());
                         newVelocities.Add(new Vector2((float) -finalVelocity, (float) -finalVelocity));
-
-                        for (int i = 0; i < 4; i++){
-                            int n = _componentDatabase.availableIds.Count;
-                            uint newId = 0;
-                            if (n != 0){
-                                newId = _componentDatabase.availableIds[n - 1];
-                                _componentDatabase.availableIds.RemoveAt(n - 1);
-                            }
-                            else {
-                                newId = _componentDatabase.entitiesCounter;
-                                _componentDatabase.entitiesCounter++;
-                            }
-
-                            _componentDatabase.UpdateSizeComponent(newId, newSize);
-                            _componentDatabase.UpdatePositionComponent(newId, newPositions[i]);
-                            _componentDatabase.UpdateVelocityComponent(newId, newVelocities[i]);
-                            _componentDatabase.UpdateIsExploded(newId, true);
-                            ecsController.CreateShape(newId, newSize);
-
-                            if (newSize <= ecsController.Config.protectionSize)
-                                _componentDatabase.UpdateIsProtectable(newId, 0);
-                        }
+                        
+                        explodedCircle = true;
                     }
+                    break;
+                }
+            }
+
+            if (explodedCircle) {
+                for (int i = 0; i < 4; i++){
+                    int n = _componentDatabase.availableIds.Count;
+                    uint newId = 0;
+                    if (n != 0){
+                        newId = _componentDatabase.availableIds[n - 1];
+                        _componentDatabase.availableIds.RemoveAt(n - 1);
+                    }
+                    else {
+                        newId = _componentDatabase.entitiesCounter;
+                        _componentDatabase.entitiesCounter++;
+                    }
+
+                    _componentDatabase.UpdateSizeComponent(newId, newSize);
+                    _componentDatabase.UpdatePositionComponent(newId, newPositions[i]);
+                    _componentDatabase.UpdateVelocityComponent(newId, newVelocities[i]);
+                    _componentDatabase.UpdateIsExploded(newId, true);
+                    ecsController.CreateShape(newId, newSize);
+
+                    if (newSize <= ecsController.Config.protectionSize)
+                        _componentDatabase.UpdateIsProtectable(newId, 0);
                 }
             }
         }
