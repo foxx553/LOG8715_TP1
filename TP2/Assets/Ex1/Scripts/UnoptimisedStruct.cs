7aadac00cd2f96b845ae09bc1df5c092921a4cab
Jérémie Cloutier-Vilhuber <jeremie@cloutier-vilhuber.net>
2025-03-12 14:49:07 -0400

    Ex1

--- TP2/Assets/Ex1/Scripts/UnoptimisedStruct.cs
+++ TP2/Assets/Ex1/Scripts/UnoptimisedStruct.cs
@@ -1,41 +1,41 @@
 using UnityEngine;
-public struct UnoptimisedStruct1
+public struct UnoptimisedStruct1                // 112 bytes
 {
-    public float velocity;
-    public bool canJump;
-    public int baseHP;
-    public int nbAllies;
-    public Vector3 position;
-    public int currentHp;
-    public float[] distancesFromObjectives;
-    public byte colorAlpha;
-    public double range;
-    public UnoptimizedStruct2 mainFriend;
-    public bool isVisible;
-    public UnoptimizedStruct2[] otherFriends;
-    public bool isStanding;
-    public float size;
-    
+    public UnoptimizedStruct2 mainFriend;       // 48 bytes
+    public float[] distancesFromObjectives;     // 8-byte
+    public UnoptimizedStruct2[] otherFriends;   // 8-byte
+    public double range;                        // 8 bytes 
+    public Vector3 position;                    // 12 bytes
+    public float size;                          // 4 bytes
+    public float velocity;                      // 4 bytes
+    public int baseHP;                          // 4 bytes
+    public int currentHp;                       // 4 bytes
+    public int nbAllies;                        // 4 bytes
+    public byte colorAlpha;                     // 1 byte
+    public bool canJump;                        // 1 byte
+    public bool isVisible;                      // 1 byte
+    public bool isStanding;                     // 1 byte
+
     public UnoptimisedStruct1(float velocity, bool canJump, int baseHP, int nbAllies, Vector3 position, int currentHp, float[] distancesFromObjectives, byte colorAlpha, double range, UnoptimizedStruct2 mainFriend, bool isVisible, UnoptimizedStruct2[] otherFriends, bool isStanding, float size)
     {
-        this.velocity = velocity;
-        this.canJump = canJump;
-        this.baseHP = baseHP;
-        this.nbAllies = nbAllies;
-        this.position = position;
-        this.currentHp = currentHp;
-        this.distancesFromObjectives = distancesFromObjectives;
-        this.colorAlpha = colorAlpha;
-        this.range = range;
         this.mainFriend = mainFriend;
-        this.isVisible = isVisible;
+        this.distancesFromObjectives = distancesFromObjectives;
         this.otherFriends = otherFriends;
-        this.isStanding = isStanding;
+        this.range = range;
+        this.position = position;
         this.size = size;
+        this.velocity = velocity;
+        this.baseHP = baseHP;
+        this.currentHp = currentHp;
+        this.nbAllies = nbAllies;
+        this.colorAlpha = colorAlpha;
+        this.canJump = canJump;
+        this.isVisible = isVisible;
+        this.isStanding = isStanding;
     }
 }
 
-public enum FriendState
+public enum FriendState : byte
 {
     isFolowing,
     isSearching,
@@ -44,30 +44,30 @@ public enum FriendState
     isJumping,
 }
 
-public struct UnoptimizedStruct2 
+public struct UnoptimizedStruct2        // 48 bytes
 {
-    public bool isAlive;
-    public float height;
-    public FriendState currentState;
-    public float velocity;
-    public int currentObjective;
-    public double maxSight;
-    public bool canJump;
-    public float acceleration;
-    public Vector3 position;
-    public float maxVelocity;
-    
+    public double maxSight;             // 8 bytes
+    public Vector3 position;            // 12 bytes
+    public float height;                // 4 bytes
+    public float velocity;              // 4 bytes
+    public float maxVelocity;           // 4 bytes
+    public float acceleration;          // 4 bytes
+    public int currentObjective;        // 4 bytes
+    public FriendState currentState;    // 1 byte
+    public bool isAlive;                // 1 byte
+    public bool canJump;                // 1 byte
+
     public UnoptimizedStruct2(bool isAlive, float height, FriendState currentState, float velocity, int currentObjective, double maxSight, bool canJump, float acceleration, Vector3 position, float maxVelocity)
     {
-        this.isAlive = isAlive;
-        this.height = height;
-        this.currentState = currentState;
-        this.velocity = velocity;
-        this.currentObjective = currentObjective;
         this.maxSight = maxSight;
-        this.canJump = canJump;
-        this.acceleration = acceleration;
         this.position = position;
+        this.height = height;
+        this.velocity = velocity;
         this.maxVelocity = maxVelocity;
+        this.acceleration = acceleration;
+        this.currentObjective = currentObjective;
+        this.currentState = currentState;
+        this.isAlive = isAlive;
+        this.canJump = canJump;
     }
 }
