15d752f17fbb5f580e0c467a8cd7744be260e5cb
Nicolas <nicolas.fleury-rousseau@polymtl.ca>
2025-03-12 16:06:31 -0400

    Ex3

--- TP2/Assets/Ex3/Scripts/MovePredatorTowardPrey.cs
+++ TP2/Assets/Ex3/Scripts/MovePredatorTowardPrey.cs
@@ -6,26 +6,63 @@ using UnityEngine;
 public class MovePredatorTowardPrey : MonoBehaviour
 {
     private Velocity _velocity;
+    [ReadOnly] public NativeArray<float3> PreyPositions;
+    [ReadOnly] public NativeArray<float3> PredatorPositions;
+    public NativeArray<float3> NearestPreyPositions;
     
     public void Start()
     {
-        _velocity = GetComponent<Velocity>();
+        PreyPositions = new NativeArray<float3>(Ex4Spawner.PreyTransforms.Length, Allocator.Persistent);
+        PredatorPositions = new NativeArray<float3>(Ex4Spawner.PredatorTransforms.Length, Allocator.Persistent);
+        NearestPreyPositions = new NativeArray<float3>(Ex4Spawner.PredatorTransforms.Length, Allocator.Persistent);
+    }
+
+    public void OnDestroy()
+    {
+        PreyPositions.Dispose();
+        PredatorPositions.Dispose();
+        NearestPreyPositions.Dispose();
     }
 
     public void Update()
     {
-        var closestDistance = float.MaxValue;
-        var closestPosition = transform.position;
-        foreach(var prey in Ex4Spawner.PreyTransforms)
+        // Copy every Prey transform to a NativeArray.
+        for (int i = 0; i < PreyPositions.Length; i++)
         {
-            var distance = Vector3.Distance(prey.position, transform.position);
-            if (distance < closestDistance)
-            {
-                closestDistance = distance;
-                closestPosition = prey.position;
-            }
+            // Vector3 is implicitly converted to float3
+            PreyPositions[i] = Ex4Spawner.PreyTransforms[i].position;
         }
 
-        _velocity.velocity = (closestPosition - transform.position) * Ex3Config.PredatorSpeed;
+        // Copy every Predator transform to a NativeArray.
+        for (int i = 0; i < PredatorPositions.Length; i++)
+        {
+            // Vector3 is implicitly converted to float3
+            PredatorPositions[i] = Ex4Spawner.PredatorTransforms[i].position;
+        }
+
+        // To schedule a job, we first need to create an instance and populate its fields.
+        MovePredatorTowardPreyJob findJob = new MovePredatorTowardPreyJob
+        {
+            PreyPositions = PreyPositions,
+            PredatorPositions = PredatorPositions,
+            NearestPreyPositions = NearestPreyPositions,
+        };
+
+        // Schedule() puts the job instance on the job queue.
+        JobHandle findHandle = findJob.Schedule(PredatorPositions.Length, 100);
+
+        // The Complete method will not return until the job represented by
+        // the handle finishes execution. Effectively, the main thread waits
+        // here until the job is done.
+        findHandle.Complete();
+        for (int i = 0; i < PredatorPositions.Length; i++)
+        {
+            Velocity predatorVelocity = Ex4Spawner.PredatorTransforms[i].GetComponent<Velocity>();
+            if (predatorVelocity != null)
+            {
+                float3 direction = NearestPreyPositions[i] - (float3)Ex4Spawner.PredatorTransforms[i].position;
+                predatorVelocity.velocity = direction * Ex3Config.PredatorSpeed;
+            }
+        }
     }
 }
